7b486905e4a37dfaf122f0ec892a717ac22e1457
anwer <theonemyth99@gmail.com>
2025-05-25 18:47:01 +0300

    hero done

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero(){
+return (
+        <div className="bg-dark text-white text-center p-5">
+            <h1>Anwer Abdelkader</h1>
+            <p>Fullstack Developer</p>
+            <button className="btn btn-light mt-3">Contact Me</button>
+            </div>
+);
+}
+export default Hero;
